4edc826084b637ad6d12f4af8c067d8dc582c103
yudayuda78 <85638796+yudayuda78@users.noreply.github.com>
2025-11-10 21:36:01 +0700

    Time: 0 ms (100.00%) | Memory: 54.7 MB (87.95%) - LeetSync

---
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function(str1, str2) {
+    const gcd = (a, b) => b === 0 ? a : gcd(b, a % b);
+    
+    if ((str1 + str2) !== (str2 + str1)) {
+        return "";
+    }
+    
+    let len = gcd(str1.length, str2.length);
+    return str1.slice(0, len);
+};
