184b8a7c5c53bcd5403c6eb1fb235e53e54a34a4
yudayuda78 <85638796+yudayuda78@users.noreply.github.com>
2025-11-27 22:15:24 +0700

    Time: 1 ms (86.13%) | Memory: 57.8 MB (71.19%) - LeetSync

--- 443-string-compression/string-compression.js
+++ 443-string-compression/string-compression.js
@@ -3,23 +3,23 @@
  * @return {number}
  */
 var compress = function(chars) {
- let write = 0;
+    let write = 0;
     let read = 0;
 
     while (read < chars.length) {
         let char = chars[read];
         let count = 0;
 
-        // hitung consecutive chars
+     
         while (read < chars.length && chars[read] === char) {
             read++;
             count++;
         }
 
-        // tulis karakter
+ 
         chars[write++] = char;
 
-        // kalau lebih dari 1, tulis angkanya per digit
+
         if (count > 1) {
             for (let c of String(count)) {
                 chars[write++] = c;
